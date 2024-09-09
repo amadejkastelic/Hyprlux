47ee186d962792978e7dbe60e860d8746d9abccc
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-09-09 21:02:19 +0200

    Split nixos and hm modules

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,12 @@
 {pkgs ? import <nixpkgs> {}}: let
-  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+  manifest = (pkgs.lib.importTOML ../Cargo.toml).package;
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = manifest.name;
     version = manifest.version;
-    cargoLock.lockFile = ./Cargo.lock;
+    cargoLock.lockFile = ../Cargo.lock;
     cargoLock.outputHashes = {
       "hyprland-0.4.0-alpha.3" = "sha256-Us7RwJbxPr0NANxyHWx7fXVyh3l8rrrX6Mw1idhHROs=";
     };
-    src = pkgs.lib.cleanSource ./.;
+    src = pkgs.lib.cleanSource ../.;
   }
---
+++ nix/module.nix
@@ -0,0 +1,116 @@
+hyprlux: {
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  time = lib.types.strMatching ''^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$'';
+
+  nightLightSubmodule = lib.types.submodule {
+    options = {
+      enabled = lib.mkOption {
+        description = "Enabled night light";
+        type = lib.types.bool;
+        default = false;
+      };
+      start_time = lib.mkOption {
+        description = "When to start night light";
+        type = time;
+        default = "20:00";
+      };
+      end_time = lib.mkOption {
+        description = "When to end night light";
+        type = time;
+        default = "06:00";
+      };
+      temperature = lib.mkOption {
+        description = "Night light temperature";
+        type = lib.types.int;
+        default = 3500;
+      };
+    };
+  };
+
+  vibranceSubmodule = lib.types.submodule {
+    options = {
+      window_class = lib.mkOption {
+        description = "Window class name or regex";
+        type = lib.types.str;
+        default = "";
+      };
+      window_title = lib.mkOption {
+        description = "Window title name or regex";
+        type = lib.types.str;
+        default = "";
+      };
+      strength = lib.mkOption {
+        description = "Vibrance strength (1-100)";
+        type = lib.types.int;
+        default = 100;
+      };
+    };
+  };
+
+  cfg = config.programs.hyprlux;
+  cfgFormat = pkgs.formats.toml {};
+
+  pkg = hyprlux.packages.${pkgs.system}.default;
+in {
+  options.programs.hyprlux = {
+    enable = lib.mkEnableOption "Enable hyprlux";
+
+    package =
+      lib.mkPackageOption pkgs "hyprlux" {}
+      // {
+        default = pkg;
+      };
+
+    night_light = lib.mkOption {
+      type = nightLightSubmodule;
+      description = "Night light settings";
+      default = {
+        enabled = false;
+        start_time = "20:00";
+        end_time = "06:00";
+        temperature = 3500;
+      };
+      example = {
+        enabled = true;
+        start_time = "20:00";
+        end_time = "06:00";
+        temperature = 3500;
+      };
+    };
+
+    vibrance_configs = lib.mkOption {
+      description = "List of vibrance configurations";
+      type = lib.types.listOf vibranceSubmodule;
+      default = [];
+      example = [
+        {
+          window_class = "^(steam_app_)(.*)$";
+          window_title = "";
+          strength = 100;
+        }
+        {
+          window_class = "Firefox";
+          window_title = "";
+          strength = 10;
+        }
+      ];
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [
+      cfg.package
+    ];
+
+    xdg.configFile."hypr/hyprlux.toml" = {
+      source = cfgFormat.generate "hyprlux.toml" {
+        night_light = cfg.night_light;
+        vibrance_configs = cfg.vibrance_configs;
+      };
+    };
+  };
+}
