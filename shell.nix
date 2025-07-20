7c63625815e154b5f3f4b29a026c182a4b62f5a7
amadejk <amadejkastelic7@gmail.com>
2025-07-20 15:18:05 +0200

    feat: make pre-commit-check optional in shell.nix
    - Add default null value for pre-commit-check parameter
    - Use conditional shellHook to support both nix-shell and nix develop
    - Enables usage without flakes while maintaining pre-commit in flake dev shell

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 {
   pkgs ? import <nixpkgs> { },
-  pre-commit-check,
+  pre-commit-check ? null,
 }:
 pkgs.mkShell {
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
@@ -10,5 +10,5 @@ pkgs.mkShell {
     rustfmt
     clippy
   ];
-  shellHook = pre-commit-check.shellHook;
+  shellHook = if pre-commit-check != null then pre-commit-check.shellHook else "";
 }
